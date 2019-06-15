3ec46d354a6b97a8dfa9390a4b03707379c8745b
tolgabuyuktanir <tolgabuyuktanir@gmail.com>
2019-06-15 20:17:35 +0300

    Extended Euclidean Algorithm for Modulo Inverse

--- eea.py
+++ eea.py
@@ -1,13 +1,12 @@
 import sys
-
-#author tolgabuyuktanir
+import math
 #Python program for Extended Euclidean algorithm
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
         gcd, x, y = egcd(b % a, a)
-        return (gcd, y - (b/a) * x, x)
+        return (gcd, y - math.floor(b/a) * x, x)
 
 def main():
     if(len(sys.argv)!=3):
